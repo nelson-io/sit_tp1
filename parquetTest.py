34a245785e1448a9032f3c4aeec422923fbaf17b
myusername <nelsonshilman@gmail.com>
2021-11-13 23:02:01 -0300

    add script comments

--- parquetTest.py
+++
@@ -1,7 +0,0 @@
-import pandas
-import pyarrow
-from pyarrow import parquet
-
-df = pandas.DataFrame({'one': [-1, 3, 2.5]},  index=list('abc'))
-table =  pyarrow.Table.from_pandas(df)
-parquet.write_table(table, 'testfile')
--- src/transform.py
+++ src/transform.py
@@ -5,10 +5,10 @@ from datetime import datetime
 from natsort import natsorted
 from pyarrow import Table as patable
 import glob
-
 from geopy import distance
 
 def build_batch(files):
+    """Reads and concats multiple parquet files into a dataframe and returns it"""
     dataframes = []
     for file in files:
         dataframes.append(pyarrow.parquet.read_pandas(file).to_pandas())
@@ -16,42 +16,59 @@ def build_batch(files):
     return batch
 
 def process_batch(batch, counter):
+    """Processes a batch dataframe, estimating the distance and time elapsed \
+        between first and last observation per group"""
 
+    # Since provided snippets didn't work, it sort values by timestamp in order 
+    # to extract max and min values per group
     batch.sort_values('timestamp', ascending = False, inplace = True)
+
+    # Add day column to DF
     batch['day'] = batch.timestamp.apply(lambda x: datetime.fromtimestamp(x).day)
 
+    # Generate DFs with first and last obs per group only
     max_time = batch.groupby(['id', 'route_short_name', 'direction', 'route_id', 'day']).head(1)
     min_time = batch.groupby(['id', 'route_short_name', 'direction', 'route_id', 'day']).tail(1)
 
+    # Merge DFs in order to associate the first API response from the batch
+    # to the last one in a tabular format
     merge_df = pd.merge(max_time, min_time , how = 'inner',
         on = ['id', 'route_short_name', 'direction', 'day'],
         suffixes= ('_max', '_min'))
 
-
+    # Estimates distance between first and last observation
     merge_df['dist'] =  [distance.distance((lat1,lon1), (lat2,lon2)).meters for lat1,lon1,lat2,lon2 in zip(merge_df.latitude_max, merge_df.longitude_max, merge_df.latitude_min, merge_df.longitude_min)]
 
+    # Estimates time elapsed between first and last observation
     merge_df['seconds'] = merge_df.timestamp_max - merge_df.timestamp_min
 
+    # Remove cases with no time elapsed
     merge_df = merge_df.loc[merge_df.seconds > 0]
 
+    # summarise data in order to remove direction from grouping variables
     merge_df = merge_df.groupby(['id','route_short_name', 'day']).agg({'dist': 'sum', 'seconds':'sum'}).reset_index() 
 
+    # Writes parquet
     parquet.write_table(patable.from_pandas(merge_df), 'out_transformed/transformed_' + str(counter) +'.parquet' )
 
     print('processed batch ' + str(counter))
 
 
 def process_transformed(files):
-    # Con la información ya generada, se pueden calcular las respuestas
+    """it processes every transformed table written in parquets, generating multiple wranglings /
+    to respond each question"""
+
+    # imports parquets into a single DF
     dataframe = build_batch(files)
 
+    #summarise the DF and estimate the 'approx' speed 
     summ_df = dataframe.groupby(['id','route_short_name', 'day']).agg({'dist': 'sum', 'seconds':'sum'}).reset_index() 
     summ_df['speed_kmh'] = (summ_df.dist/1000) / (summ_df.seconds / 3600)
 
     
+    # Generate and print responses
 
-
-    print('Velocidad por día por línea')
+    print('speed by route_short_name ')
     res_0 = summ_df.groupby(['route_short_name','day']).agg({'speed_kmh':'max'}).reset_index
     print(res_0)
 
@@ -75,15 +92,6 @@ def process_transformed(files):
     res_5 = summ_df.sort_values('speed_kmh',ascending=False).groupby(['route_short_name']).tail(1).reset_index().loc[:,['route_short_name', 'day']]
     print(res_5)
 
-
-    # Hay que agrupar por las dimensiones necesarias y aplicar funciones de agregación:
-    # dataframe.groupby(['route_short_name','day']).seconds.sum()
-    # dataframe.groupby(['route_short_name','day']).distance.sum()
-    # Y así calcular la velocidad de cada bloque
-
-    # Imprimir las respuestas para
-    # - velocidad por día de cada línea
-    # - interno más rápido de cada línea
 
 if __name__ == '__main__':
     input_path = 'out/*.parquet'
